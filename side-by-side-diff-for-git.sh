d15a21b3eb68617796a64c66801339f08ec381dc
Gauthier Östervall <gauthier.ostervall@lumenradio.com>
2017-09-27 11:06:57 +0200

    Use icdiff for side-by-side diff if present

--- side-by-side-diff-for-git.sh
+++ side-by-side-diff-for-git.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-if type colordiff 1> /dev/null 2>&1 ; then
+if type icdiff 1> /dev/null 2>&1 ; then
+    icdiff --line-numbers --highlight --recursive "$2" "$1"
+elif type colordiff 1> /dev/null 2>&1 ; then
     colordiff -y "$1" "$2" --width=164
 else
     echo "colordiff not installed, using `diff` instead"
