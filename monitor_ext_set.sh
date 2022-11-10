d5423342d48b32d148a51ef29a990d348a748972
Gauthier Östervall <gauthier.ostervall@lumenradio.com>
2022-11-10 14:49:22 +0100

    More automated external monitor setup

--- monitor_ext_set.sh
+++ monitor_ext_set.sh
@@ -26,7 +26,29 @@ xrandr --output $MONITOR_LEFT --auto
 
 if [ $NB_CONNECT_OUTPUT == 3 ] ; then
     xrandr \
-	--output $MONITOR_LEFT --pos 1680x0 --mode 1920x1200 --auto --primary \
+	--output $MONITOR_LEFT --auto --primary \
         --output $MONITOR_RIGHT --right-of $MONITOR_LEFT --auto \
-	--output $MONITOR_LAPTOP --auto --pos 0x150 --mode 1680x1050
+	--output $MONITOR_LAPTOP --auto --right-of $MONITOR_RIGHT --mode 1680x1050
+fi
+
+if [ $NB_CONNECT_OUTPUT == 2 ] ; then
+    # dock with single monitor, don't use laptop
+    xrandr \
+	--output $MONITOR_LAPTOP --off \
+        --output $MONITOR_LEFT --auto
+fi
+
+if [ $NB_CONNECT_OUTPUT == 1 ] ; then
+    for m in ${ALL_MONITORS[@]} ; do
+	echo "Turning off monitor $m"
+	xrandr --output $m --off
+    done
+
+    # dock with single monitor, don't use laptop
+    xrandr --output $MONITOR_LAPTOP --auto --primary
+fi
+
+if type compton; then
+    # start compton if not running. xrandr seems to crash it sometimes.
+    pgrep compton || compton --config "$HOME/.xmonad/compton.conf" &
 fi
